d3ea110dc237204c88f04fa844248af69da88ed1
Fabrício Almeida <fabriciomailcontato@gmail.com>
2025-10-01 23:42:15 -0300

    Make HeroSection able to accept parameters

--- src/components/Herosection/HeroSection.jsx
+++ src/components/Herosection/HeroSection.jsx
@@ -1,10 +1,9 @@
 import React from "react";
 import uane_fdr_logos from "../../img/uane_fdr_logos.png"
-import course_picture from "../../img/course_picture.png"
 //import "bootstrap/dist/css/bootstrap.min.css";
 import style from "./HeroSection.module.css"; // custom styles
 
-const HeroSection = () => {
+const HeroSection = ({ title, description, picture, tags}) => {
   return (
     <section className="py-5" style={{ backgroundColor: "#2a084c" }}>
       <div className="container">
@@ -12,19 +11,13 @@ const HeroSection = () => {
           {/* Left side */}
           <div className="col-lg-6 text-white">
             <div className="mb-3">
-              <span className="badge btn btn-outline-light text-light me-2">TÉCNICO</span>
-              <span className="badge btn btn-outline-light text-light">TECNOLOGIA E PROFISSÃO</span>
+              <span className="badge btn btn-outline-light text-light me-2"> {tags[0]} </span>
+              <span className="badge btn btn-outline-light text-light"> {tags[1]} </span>
             </div>
 
-            <h2 className="fw-bold">Secretaria Escolar</h2>
+            <h2 className="fw-bold"> {title} </h2>
             <p className="mt-3">
-              O(a) profissional em Secretaria Escolar é peça-chave para a gestão eficiente
-              da escola, garantindo o funcionamento legal e organizado da instituição.
-              Responsável por gerenciar a documentação dos estudantes, ele(a) também
-              contribui para o alinhamento do currículo escolar com as necessidades reais
-              dos alunos. Seja essencial para o sucesso da escola e para a formação de um
-              ambiente educacional estruturado. Invista na sua capacitação e faça a
-              diferença na gestão escolar!
+              {description}
             </p>
 
           <div className="row align-items-center mt-4"> 
@@ -43,7 +36,7 @@ const HeroSection = () => {
           {/* Right side */}
           <div className="col-lg-6 text-center mb-3 mb-lg-0 mt-lg-0 order-first order-lg-last">
             <img
-              src={course_picture}
+              src={picture}
               alt="Profissional Secretaria Escolar"
               className="img-fluid rounded"
             />
